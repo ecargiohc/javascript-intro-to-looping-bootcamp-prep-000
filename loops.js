9bfb32d0bb45acf572cea60aac6cadaa402af852
gc6123 <ilovegrace09@gmail.com>
2019-06-09 21:16:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,3 +22,37 @@ function doWhileLoop(array) {
   while (false)
   return array
 }
+
+function forLoop (array) {
+  for (let i=0; i<25; i++)
+    if (i===1) {
+      array.push ("I am 1 strange loop.")
+    }
+    else {
+      array.push ('I am ${i} strange loops.')
+    }
+    return array;
+  }
+
+function whileLoop(number) {
+  let countdown = number
+while (countdown > 0) {
+  console.log(--number);
+  return ('done');
+}  
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log("I run once regardless.");
+  } while
+  (incrementVariable() < num );
+}
+
+
+
+
+
+
+
+
